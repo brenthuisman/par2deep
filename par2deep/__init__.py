184b513c25ab9cee670263390265254d2100e0b8
Brent Huisman <mail@brenthuisman.net>
2016-08-07 00:05:11 +0200

    .

--- par2deep/__init__.py
+++ par2deep/__init__.py
@@ -211,7 +211,13 @@ def main():
 
 	## Final Report
 	print("Finished.")
-	print("There were",len(all_err),"errors,",len(repairedfiles),"succesful fixes,",len(removedfiles),"files removed and",len(recreatedfiles),"overwritten new parity files.")
+	print("==========================================================")
+	print("There were:")
+	print(len(createdfiles),"newly created parity files.")
+	print(len(all_err),"errors.")
+	print(len(repairedfiles),"succesful fixes")
+	print(len(removedfiles),"files removed.")
+	print(len(recreatedfiles),"overwritten new parity files.")
 
 	if len(all_err)>0:
 		return 1
